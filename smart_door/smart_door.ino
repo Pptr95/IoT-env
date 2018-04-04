47d89269aadd2408f2030868e6540c420582e1c5
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-04-04 17:36:38 +0200

    add task in .ino, bug pins numbers

--- smart_door/smart_door.ino
+++ smart_door/smart_door.ino
@@ -1,9 +1,11 @@
 #include "FlexiTimer2.h"
 #include "AuthTask.h"
-
+#include "EnvTask.h"
 #define OMEGA 100
+//AuthTask *authTask = new AuthTask(9, 7, 4, 6, 8, 4, 0, 2, 10, 12);   //servo, trig, echo, ledOn, ledValue, btnExit, temp, rxd(bt), txd(bt), pir
+AuthTask *authTask = new AuthTask(7, 4, 6, 2, 10);   //trig, echo, ledOn,  rxd(bt), txd(bt)
+EnvTask *envTask = new EnvTask(9, 8, 4, 0, 12);   //servo, ledValue, btnExit, temp, pir
 
-AuthTask *authTask = new AuthTask(9, 7, 4, 6, 8, 4, 0, 2, 10, 12);   //servo, trig, echo, ledOn, ledValue, btnExit, temp, rxd(bt), txd(bt), pir
 
 void runTask() {
   authTask->tick();
@@ -17,3 +19,4 @@ void setup() {
 
 void loop() {
 }
+
