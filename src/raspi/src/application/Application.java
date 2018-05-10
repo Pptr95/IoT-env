54bc824bc0ff0a0be11c61b71cf898d4447e5a91
Breiker <Breiker@MacBook-Pro-di-Giulio.local>
2018-05-10 14:44:59 +0200

    Fixed led, resourceBundle and fritzing

--- src/raspi/src/application/Application.java
+++ src/raspi/src/application/Application.java
@@ -9,7 +9,7 @@ import utils.ServerUDP;
 public class Application {
 	public static void main(String[] args) throws Exception {
 		Light ledInside = new Led(1);
-		Light ledFailed = new Led(23);
+		Light ledFailed = new Led(4);
 		InputMsgReceiver serial = new InputMsgReceiver(args[0], Integer.valueOf(args[1]));
 		ServerUDP server = new ServerUDP();
 		SmartDoor sr = new SmartDoor(serial, server, ledInside, ledFailed);
--- src/raspi/src/utils/Resource.java
+++ src/raspi/src/utils/Resource.java
@@ -1,24 +1,22 @@
 package utils;
 
 import java.io.Serializable;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
-
 public class Resource implements Serializable {
-  
-    private static final long serialVersionUID = -3325080430848316075L;
-    private final ResourceBundle res;
-   
-    public Resource() {
-        this.res = ResourceBundle.getBundle("Users", Locale.getDefault());
-    }
-    
-    public boolean checkUserExist(final String username) {
-    		return this.res.containsKey(username);
-    }
-    
-    public String getPassword(final String username) {
-        return this.res.getString(username);
-    }
+
+	private static final long serialVersionUID = -3325080430848316075L;
+	private final ResourceBundle res;
+
+	public Resource() {
+		this.res = ResourceBundle.getBundle("Users");
+	}
+
+	public boolean checkUserExist(final String username) {
+		return this.res.containsKey(username);
+	}
+
+	public String getPassword(final String username) {
+		return this.res.getString(username);
+	}
 }
